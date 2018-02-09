ed9e0f917e0c352bd826e4795619706ab777f761
Jace Mitsuka <PJace@BigMac.local>
2018-02-08 20:32:59 -1000

    exercise 8

--- exercise.js
+++ exercise.js
@@ -89,7 +89,9 @@ console.log(oneLove);
 
 var shoe = ["just", "do", "it"];
 
+var shoeString = shoe.join();
 
+console.log(shoeString);
 
 /*The array.slice() extracts a section of an array and returns a new array.
 array.slice(index start, index end)*/
